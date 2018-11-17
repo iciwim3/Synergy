8b5763849f11da85c22cfe5cec18c8803fb62399
SKE <appybuildmore@gmail.com>
2018-11-16 19:51:30 -0800

    Initial Commit

---
+++ Synergy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Synergy
+//
+//  Created by Sain-R Edwards on 11/16/18.
+//  Copyright © 2018 Swift Koding 4 Everyone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
